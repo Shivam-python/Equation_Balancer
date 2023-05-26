2f94b7bd5da0a5bebbe459b52706582c0c448864
Shivam-python <raynoldw222@gmail.com>
2023-05-26 17:45:22 +0530

    fix: removed sqlite DB

--- equationbalancer/settings.py
+++ equationbalancer/settings.py
@@ -75,10 +75,7 @@ WSGI_APPLICATION = 'equationbalancer.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+
 }
 
 
