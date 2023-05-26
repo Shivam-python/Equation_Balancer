2094dc75f9a922263894905793d0a9c0a3f45c5f
Shivam-python <raynoldw222@gmail.com>
2023-05-26 17:41:33 +0530

    feat: added vercel files & static files
    Added required files for deploying the django app on vercel.

--- equationbalancer/settings.py
+++ equationbalancer/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'syg*ghn4t_xpju!@wqq9rv+s+axc^l9ff4!=o$jmrh9wa0y%6g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'templates'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
